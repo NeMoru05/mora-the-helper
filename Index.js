a492c621ee64f40324411e5ac16ba9e0457fa3b7
SleepyMoru <87416908+NeMoru05@users.noreply.github.com>
2023-01-17 07:51:59 +0800

    Sleepy Moru

--- Index.js
+++ Index.js
@@ -2,16 +2,10 @@
 /* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 const { Client, Events, GatewayIntentBits } = require ('discordjs');
-const { TOKEN } = require ('dotenv');
+const { TOKEN } = require ('.env');
 
 const client = new Client({ intents: [
                                     GatewayIntentBits.Guilds, 
                                     GatewayIntentBits.GuildMessages, 
                                     GatewayIntentBits.MessageContent, 
                                     GatewayIntentBits.GuildMembers] });
-
-client.once(Events.ClientReady, c => {
-    console.log('Mora At Ur Service');
-});
-
-client.login();
